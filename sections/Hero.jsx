92ba664c145faeb7a6ee502205d2d84182010dbf
shaan-hossain-01 <rockshaan200@gmail.com>
2025-08-19 17:52:21 +0600

    Added Hero Section

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,3 +1,22 @@
 export default function Hero() {
-    return <div>Hero</div>;
+  return (
+    <section>
+      <div className="container py-24">
+        <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-500 rounded-full">
+          7.5M round seed round raised
+        </div>
+        <h1>Impactful design, meaningful outcomes</h1>
+        <p>
+          We help you create products that not only look good but also deliver
+          real value to your users. Our team of experts is dedicated to
+          understanding your unique needs and crafting tailored solutions that
+          drive results.
+        </p>
+        <form>
+          <input type="email" placeholder="Enter your email" />
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+    </section>
+  );
 }
