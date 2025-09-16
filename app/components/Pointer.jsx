59b5342520b7aee29e23b168cf9d646f29368f34
Atiq Afzal Hossain Masud <rockshaan200@gmail.com>
2025-09-16 16:31:15 +0600

    Fixed all issues and made pixel perfect Hero Section

--- app/components/Pointer.jsx
+++ app/components/Pointer.jsx
@@ -1,3 +1,26 @@
-export default function Pointer(){
-    
+import {twMerge} from "tailwind-merge";
+
+export default function Pointer({name, color}) {
+  return (
+    <div className="relative">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="cursor-pointer text-white size-5"
+      >
+        <path d="M3 3l7.07 16.97 2.51-7.39 7.39-2.51L3 3z"></path>
+        <path d="M13 13l6 6"></path>
+      </svg>
+      <div className="absolute top-full left-full">
+        <div className={twMerge("inline-flex rounded-full rounded-tl-none font-bold text-sm px-2 bg-blue-500 ", color === "red" && "bg-red-500")}>{name}</div>
+      </div>
+    </div>
+  );
 }
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,4 +1,5 @@
 import Button from "@/app/components/Button";
+import Pointer from "@/app/components/Pointer";
 import designExample1Image from "@/app/assets/images/design-example-1.png";
 import designExample2Image from "@/app/assets/images/design-example-2.png";
 import Image from "next/image";
@@ -7,19 +8,25 @@ export default function Hero() {
   return (
     <section className=" py-24 overflow-x-clip">
       <div className="container relative mx-auto">
-        <div className="absolute top-16 -left-32">
+        <div className="absolute top-16 -left-52 hidden lg:block">
           <Image
             src={designExample1Image}
             alt="Design Example 1"
             className=""
           />
         </div>
-        <div className="absolute -right-64 -top-16">
+        <div className="absolute -right-86 -top-16 hidden lg:block">
           <Image
             src={designExample2Image}
             alt="Design Example 2"
             className=""
           />
+        </div>
+        <div className="absolute left-38 top-105 hidden lg:block">
+          <Pointer name={"Andrea"} />
+        </div>
+        <div className="absolute right-60 -top-4 hidden lg:block">
+          <Pointer name={"Bryan"} color={"red"} />
         </div>
         <div className="flex items-center justify-center">
           <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-500 rounded-full font-semibold text-neutral-950 ">
@@ -27,9 +34,10 @@ export default function Hero() {
           </div>
         </div>
         <h1 className="text-6xl md:text-7xl lg:text-8xl font-medium text-center mt-6">
-          Impactful design, meaningful outcomes
+          Impactful design,
+          <br /> created effortlessly
         </h1>
-        <p className="text-xl text-center text-white/50 mt-8 max-w-2xl mx-auto">
+        <p className="text-xl text-center text-white/50 mt-8 max-w-2xl mx-auto px-4">
           We help you create products that not only look good but also deliver
           real value to your users. Our team of experts is dedicated to
           understanding your unique needs and crafting tailored solutions that
